fb0bf1ded76ea747611a4372f26223348ab02bf0
Eliot <42266703+smorad3363@users.noreply.github.com>
2026-09-06 21:37:31 +0330

    Define Xray relay control protocol

---
+++ internal/xrayrelay/protocol.go
@@ -0,0 +1,26 @@
+package xrayrelay
+
+import "github.com/egress-manager/egress-manager/internal/domain"
+
+type ImportRequest struct {
+	Input string `json:"input"`
+}
+
+type ImportResponse struct {
+	Outbounds []domain.Outbound `json:"outbounds"`
+}
+
+type TestRequest struct {
+	ID               domain.ID `json:"id,omitempty"`
+	ExpectedRevision int64     `json:"expected_revision,omitempty"`
+	Input            string    `json:"input,omitempty"`
+}
+
+type TestResult struct {
+	Outbound domain.Outbound       `json:"outbound"`
+	Health   domain.OutboundHealth `json:"health"`
+}
+
+type TestResponse struct {
+	Results []TestResult `json:"results"`
+}
